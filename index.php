777e4ab2a2c1f361daeb6bfc8c418b1278239924
Fahru27 <19523005@students.uii.ac.id>
2021-01-27 13:46:04 +0700

    masok gan

---
+++ index.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- css -->
+    <link rel="stylesheet" href="style2.css">
+    <title>ProjectGan</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">ProjectGan</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav" style="margin-left: auto;">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="jumbotron jumbotron-fluid">
+            <div id="text">
+                <h1>build your website<br>with "project gan"</h1>
+                <p>Providing good result with fair price</p>
+                <a href="">order now</a>
+            </div>
+
+            <div>
+                <img src="images/app_development_PNG.png" alt="">
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section id="about">
+            <article id="tentang">
+                <h2>Tentang ProjectGan</h2>
+                <p>Project Gan terbentuk dari sekelompok mahasiswa gabut jurusan informatika Universitas Islam Indonesia. Kami menawarkan jasa pembuatan web dengan harga yang dapat anda atur sendiri. Hanya disini anda bisa mendapatkan jasa pembuatan web dengan harga sesuai keinginan anda.</p>
+            </article>
+
+            <h2>Mengapa ProjectGan?</h2>
+            <article id="mengapa">
+                <div>
+                    <img src="images/Online payment _Two Color (3).png" alt="">
+                    <div>
+                        <h3>Harga Bersahabat</h3>
+                        <p>Dengan harga yang dijamin lebih murah dari yang lain, anda bisa mendapatkan website dengan kualitas dan keamanan terjamin.</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Pengerjaan Cepat</h3>
+                        <p>Satu project dikerjakan dengan 4 developer sehingga waktu pengerjaan lebih cepat dan efisien.</p>
+                    </div>
+                    <img src="images/Fast Working_Monochromatic (1).png" alt="">
+                </div>
+                <div>
+                    <img src="images/Security_Isometric (2).png" alt="">
+                    <div>
+                        <h3>Keamanan Terjamin</h3>
+                        <p>Tentu saja dengan menggunakan ssl, anda akan lebih aman untuk menyimpan data pribadi di website anda.</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+
+        <section id="tim">
+            <h2>Tim Kami</h2>
+            <div>
+                <div>
+                    <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/671/10689/apizaqhdp__82079.1605627908.jpg?c=1" alt="">
+                    <p>Alif Maulana R.</p>
+                </div>
+                <div>
+                    <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/671/10689/apizaqhdp__82079.1605627908.jpg?c=1" alt="">
+                    <p>Fahrudin Nasikh A.</p>
+                </div>
+                <div>
+                    <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/671/10689/apizaqhdp__82079.1605627908.jpg?c=1" alt="">
+                    <p>M. Ilham Rizkyakbar</p>
+                </div>
+                <div>
+                    <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/671/10689/apizaqhdp__82079.1605627908.jpg?c=1" alt="">
+                    <p>Naufal Yusran</p>
+                </div>
+            </div>
+            <p>Kami semua merupakan mahasiswa informatika dari salah satu universitas di Yogyakarta. Kami siap memberikan layanan terbaik kepada anda.</p>
+        </section>
+
+        <section id="portofolio">
+            <h2>Portofolio</h2>
+        </section>
+
+    </main>
+
+    <footer>
+        <div class="container">
+            <h2 id="h2contact">Contact Us</h2>
+            <section id="contact">
+                <div>
+                    <p>Hubungi kami sekarang dan buat<br>website anda</p>
+
+                    <div id="sosial">
+                        <img src="images/toppng.com-facebook-transparent-logo-png-1600x1600-1600x1600.png" alt="">
+                        <img src="images/toppng.com-gmail-icon-android-lollipop-512x512.png" alt="">
+                        <img src="images/toppng.com-instagram-pn-599x600.png" alt="">
+                        <img src="images/toppng.com-whatsapp-transparent-p-1012x1024.png" alt="">
+                    </div>
+
+                    <p>Jl. Kaliurang KM 12.5, Sardonoharjo,<br>Nganglik, Sleman, D.I Yogyakarta.<br><br>WA/Telp: 082238248899<br>Email: projectgan@gmail.com</p>
+                </div>
+
+                <div>
+                    <form action="">
+                        <input type="text" name="nama" id="nama" placeholder="Name">
+                        <input type="email" name="email" id="email" placeholder="Email">
+                        <input type="text" name="notelp" id="notelp" placeholder="Phone">
+                        <textarea name="pesan" id="pesan" rows="10" placeholder="Message"></textarea>
+
+                        <button>Send</button>
+                    </form>
+                </div>
+            </section>
+        </div>
+
+        <div class="container" id="copyright">
+            <p>Developed by ProjectGan Copyright &copy; 2021</p>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+
+    <script>
+        $(window).scroll(function() {
+            $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
+        });
+    </script>
+</body>
+
+</html>
